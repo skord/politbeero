1178ff15506a1d47ade7d2b60cae9db8c405398e
Mike Danko <mike@l4m3.com>
2013-06-05 19:03:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politbeero::Application.config.secret_token = '4c2821dfb7374188b9001ce32448cdc0d091cb99d0afb2e4d17e54f64f4c561fad59c15f637e31ececd2ac3e1c51cba0db616f0e3b41e680438a45b317307b73'
